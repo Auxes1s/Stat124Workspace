43463d12104a0f7195f59cadf4ee5388346fb188
Marc Ignacio <muignacio@up.edu.ph>
2020-10-28 11:40:06 +0800

    Added explanation and code readability

--- Week 7/STAT124_TASK7_WFV_pr0grammers.R
+++ Week 7/STAT124_TASK7_WFV_pr0grammers.R
@@ -6,34 +6,50 @@
 
 #Queston 1 -----
 container = 1:50
+
+#ELSEIF Statements check if a number satisfies the conditions for BEEP or BOOP or BEEPBOOP
 for(i in container){
-  if((i %% 2 ==0)& (i %% 3 != 0)){
+  if((i %% 2 == 0)& (i %% 3 != 0)){
     cat("Beep\n")
-  }else if ((i %% 3 ==0)& (i %% 2 != 0)){
+  }else if ((i %% 3 == 0)& (i %% 2 != 0)){
     cat("Boop\n")
-  }else if ((i %% 3 ==0)& (i %% 2 == 0)){
+  }else if ((i %% 3 == 0)& (i %% 2 == 0)){
     cat("BeepBoop\n")
   } else {
     cat(paste(i,"\n"))
   }
 }
 
+
 #Queston 2 -----
+
 #userinp = readline("How many asterisks do you want master?:")
 userinp = 6
-for(i in seq(as.integer(userinp))){
+
+#creates ascending sequence from 1 to the nth term.
+ascending = seq(as.integer(userinp)) 
+#creates descending sequence from nth term integers to 1.
+descending = seq(as.integer(userinp), by = -1)[-1]
+for(i in ascending){
   cat(rep("*",i))
   cat("\n")
 }
-for(i in seq(as.integer(userinp),by = -1)[-1]){
+for(i in descending){
   cat(rep("*",i))
   cat("\n")
 }
 
+
 #Queston 3 -----
+#Takes user input
 userinp = readline("What do you want to reverse master?:")
+#Splits user input of string into a list of the characters
 splitted = strsplit(userinp, split = "")
+#Creates a sequence of descending integers for indexing
 rev = nchar(userinp):1
+#Uses the created sequence and indexes splitted
 reversed_chars = splitted[[1]][rev]
+#Joins the list of characters into one string
 reversed_chars = paste(reversed_chars, collapse = "")
-cat(paste("The reverse of ",userinp," is ",reversed_chars,".",sep=""))
+
+cat("The reverse of ",userinp," is ",reversed_chars,".", sep="")
