7ecf501615e0eb07776a82efd82ea8cd6fe6056b
Auxes1s <muignacio@up.edu.ph>
2020-11-14 20:33:17 +0800

    Week 11-12

---
+++ Week 11-12/STAT124_TASK10_WFV_pr0grammers.R
@@ -0,0 +1,66 @@
+# Members:
+# Tuyogon, Ann
+# Batac, John Kenneth
+# Ignacio, Marc Shervin
+
+
+#Question 1 ----
+trianglr = function(h){
+  #creates ascending sequence from 1 to the nth term.
+  ascending = seq(as.integer(h)) 
+  #creates descending sequence from nth term integers to 1.
+  descending = seq(as.integer(h), by = -1)[-1]
+  
+  for(i in ascending){
+    cat(rep("*",i))
+    cat("\n")
+  }
+    
+  for(i in descending){
+    cat(rep("*",i))
+    cat("\n")
+  }
+}
+
+#Question 2 ----
+trianglr = function(h){
+  if((h < 2) || (h > 10)){
+    stop("The function only takes integers from 2 to 10 only.")
+  }
+  #creates ascending sequence from 1 to the nth term.
+  ascending = seq(as.integer(h)) 
+  #creates descending sequence from nth term integers to 1.
+  descending = seq(as.integer(h), by = -1)[-1]
+  
+  for(i in ascending){
+    cat(rep("*",i))
+    cat("\n")
+  } 
+    
+  for(i in descending){
+    cat(rep("*",i))
+    cat("\n")
+  }
+}
+
+#Question 3 ----
+trianglr = function(h=5){
+  if((h < 2) || (h > 10)){
+    stop("The function only takes integers from 2 to 10 only.")
+  }
+  #creates ascending sequence from 1 to the nth term.
+  ascending = seq(as.integer(h)) 
+  #creates descending sequence from nth term integers to 1.
+  descending = seq(as.integer(h), by = -1)[-1]
+  
+  for(i in ascending){
+    cat(rep("*",i))
+    cat("\n")
+  }
+    
+  for(i in descending){
+    cat(rep("*",i))
+    cat("\n")
+  }
+}
+
